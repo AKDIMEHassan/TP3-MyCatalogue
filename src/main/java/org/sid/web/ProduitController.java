4fe68ec6b758aace8a502ac1604e0e8d07549fe9
AKDIMEHassan <hassan.akdime.05@gmail.com>
2022-01-09 09:50:11 +0100

    commit

---
+++ src/main/java/org/sid/web/ProduitController.java
@@ -0,0 +1,26 @@
+package org.sid.web;
+
+import java.util.List;
+
+import org.sid.dao.ProduitRepository;
+import org.sid.entities.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProduitController {
+	@Autowired
+	private ProduitRepository produitRepository;
+    @GetMapping(path="/index")
+    public String  index() {
+    	return "index";
+    }
+    @GetMapping(path="/products")
+    public String  products(Model model) {
+    	List<Produit> produits=produitRepository.findAll();
+    	model.addAttribute("listProduits",produits);
+    	return "products";
+    }
+}
